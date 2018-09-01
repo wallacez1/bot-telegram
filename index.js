5e70c6a51d9909c0f10ebd07d2e0d988e294cafd
wallace <wallace_silva@unigranrio.br>
2018-09-01 00:18:40 -0300

    adequacoes no index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
+const server = require('server');
 
+const { get, post } = server.router;
+
+// Launch server
+server({ port: 3000 }, [  
+    get('/', ctx => 'Hello world!')
+]);
 
 server.listen(process.env.PORT || port)
 
