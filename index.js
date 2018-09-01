e631eb9f5d8589c92801ce29f4db5ab60f7dc664
wallace <wallace_silva@unigranrio.br>
2018-08-31 23:48:49 -0300

    adequacoes no index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 'use strict'
-require('dotenv').config();
-var express = require('express')
-var app = express();
+
+
 
 const Telegram = require('telegram-node-bot')
 const tg = new Telegram.Telegram(process.env.TELEGRAM_KEY, {
     workers: 1
 })
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+
 
 const TrafficInfoController = require('./controllers/traffic')
 const OtherwiseController = require('./controllers/otherwise')
