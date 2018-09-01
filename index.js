2ebb9d3c868b29a9999257ca0b491ed9222b8491
wallace <wallace_silva@unigranrio.br>
2018-08-31 23:42:30 -0300

    adequacoes no index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const tg = new Telegram.Telegram(process.env.TELEGRAM_KEY, {
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
