489eda205b68ac580b9358ce1803636188a55fc5
wallace <wallace_silva@unigranrio.br>
2018-09-01 00:37:53 -0300

    adequacoes no index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express')
 var app = express()
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
