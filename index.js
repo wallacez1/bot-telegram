4fd163ff015611974fb99f73ff3e40ac93b9f310
wallace <wallace_silva@unigranrio.br>
2018-08-31 23:15:48 -0300

    adequacoes no server

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 
 require('dotenv').config();
-var express = require('express')
-var app = express();
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 const Telegram = require('telegram-node-bot')
 const tg = new Telegram.Telegram(process.env.TELEGRAM_KEY, {
     
 })
 
-app.listen(process.env.PORT || 5000)
 
 const TrafficInfoController = require('./controllers/traffic')
 const OtherwiseController = require('./controllers/otherwise')
